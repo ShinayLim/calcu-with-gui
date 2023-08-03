da625c9363111e8c7b98b01c3a307c2b9ac35a20
Shin I. Lim <92615780+ShinayLim@users.noreply.github.com>
2023-08-03 12:17:16 +0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 
+largeFontStyle = ("Arial", 40)
+smallFontStyle = ("Arial", 16)
 lightGray = "#f5f5f5"
+labelColor = "#25265e"
 
 class Calculator:
     def __init__(self):
@@ -9,8 +12,22 @@ class Calculator:
         self.window.resizable(0,0)
         self.window.title("Calculator by ShinayLim")
 
+        self.totalExpression = "0"
+        self.currentExpression = "0"
         self.displayFrame = self.createDisplayFrame()
+
+        self.totalLabel, self.label = self.createDisplayLabels
         self.buttonsFrame = self.createButtonsFrame()
+
+    def createDisplayLabels(self):
+        totalLabel = tk.Label(self.displayFrame, text=self.totalExpression, 
+                              anchor=tk.E, bg=labelColor, padx=24, font=smallFontStyle)
+        totalLabel.pack(expand=True, fill="both")
+
+        label = tk.Label(self.displayFrame, text=self.currentExpression, 
+                              anchor=tk.E, bg=labelColor, padx=24, font=largeFontStyle)
+        label.pack(expand=True, fill="both")
+        return totalLabel.label
 
     def createDisplayFrame(self):
         frame = tk.Frame(self.window, height=221, bg=lightGray)
