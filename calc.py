58287830bedb2aa78807c1ff4735f60cbb66ab10
Shin I. Lim <92615780+ShinayLim@users.noreply.github.com>
2023-08-03 13:45:01 +0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -38,7 +38,7 @@ class Calculator:
         self.window = tk.Tk()
         self.window.geometry("375x667")
         self.window.resizable(0, 0)
-        self.window.title("Calculator")
+        self.window.title("Calculator by ShinayLim")
 
         self.theme = LIGHT_THEME
         self.totalExpression = ""
